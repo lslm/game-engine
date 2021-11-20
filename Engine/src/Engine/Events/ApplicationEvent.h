539c8e059d041529fb7f08b928164c48bff3e341
Lucas Santos <lucas.santos.silva2011@gmail.com>
2021-11-20 14:43:28 -0300

    Added WindowCloseEvent

--- Engine/src/Engine/Events/ApplicationEvent.h
+++ Engine/src/Engine/Events/ApplicationEvent.h
@@ -27,4 +27,13 @@ namespace Engine
     private:
         unsigned int m_Width, m_Height;
     };
+    
+    class WindowCloseEvent : public Event
+    {
+    public:
+        WindowCloseEvent() = default;
+        
+        EVENT_CLASS_TYPE(WindowClose);
+        EVENT_CLASS_CATEGORY(EventCategoryApplication);
+    };
 }
