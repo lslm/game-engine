1fe7cdc3f5c1e05c0c8f843aa42de619288aa0ed
Lucas Santos <lucas.santos.silva2011@gmail.com>
2021-11-28 01:04:42 -0300

    Silence deprecation warnings

--- Engine/src/Engine/Application.cpp
+++ Engine/src/Engine/Application.cpp
@@ -47,7 +47,7 @@ namespace Engine {
         ENGINE_CORE_INFO("GLFW initialized");
         while (m_Running)
         {
-            glClearColor(1, 0, 1, 1);
+            glClearColor(0.1f, 0.3f, 0.3f, 1.0f);
             glClear(GL_COLOR_BUFFER_BIT);
             
             for(Layer* layer : m_LayerStack)
